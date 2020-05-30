3d85066244b4148546ece3500e3564bcbcf65bf5
KettarKat <kettarkat@gmail.com>
2020-05-30 23:01:54 +0300

    added product-slider section

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.product__slider-items').slick({
+    prevArrow: '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
+    nextArrow: '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
+  });
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "15px",
+    ratedFill: "#ffc000",
+    normalFill: "#fff",
+    spacing: "3px",
+  });
+   
+  
+})
