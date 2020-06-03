b2ad0bf355130058db7fbee1a3e0f0fd428be8a2
KettarKat <kettarkat@gmail.com>
2020-06-03 12:51:39 +0300

    added followers feed section and icons-info section

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,14 @@ $(function(){
     prevArrow: '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
     nextArrow: '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
   });
+
+  $('.followers__slider-items').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
+    nextArrow: '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
+  });
+  
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "15px",
